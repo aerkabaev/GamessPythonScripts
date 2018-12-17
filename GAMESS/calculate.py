4aa8b1bfaaa388a3f8e3ad6d67688d4f4f3cc200
a.erkabaev <a.erkabaev@linscaninspection.com>
2018-12-17 13:54:54 +0500

    more refactoring and logic

--- GAMESS/calculate.py
+++ GAMESS/calculate.py
@@ -1,28 +1,36 @@
-import input
+import inputfiles
+import subprocess
+
 
 class Calculate(object):
     def __init__(self, structure_file_path, template, gamess_command_path, gamess_string):
         self.template = template
         self.gamess_command_path = gamess_command_path
         self.gamess_string = gamess_string
+        self.input = inputfiles.InputFiles()
         with open(structure_file_path, 'r') as file:
             self.input_structure = file.read()
-        
-    def CheckOptimizeHessian(self, input_structure_path, method):
 
-        # parse structure file
-        name = ''
-        symmetry = ''
-        geometry = ''
+    def single_run(self, input_file_path, output_file_path, cmd_file_path, rule_list):
+        # create input files
+        self.input.create_input(input_file_path, rule_list)
+        self.input.create_run_command(cmd_file_path, input_file_path, output_file_path)
 
-        name_rule = ('*Name*', name)
-        symmetry_rule = ('*Symmetry*', symmetry)
-        geometry_rule = ('*Geometry*', geometry)
+        # run
+        subprocess.check_call(cmd_file_path, shell=True)
 
-        inp = input.InputFiles(template, )
-        # create check input
+        # check
+        with open(output_file_path, 'r') as file:
+            f = file.read()
+            success = 'EXECUTION OF GAMESS TERMINATED NORMALLY' in f
+
+        if not success:
+            raise Exception('Calculation error')
+
+    def run(self, file_name, folder, method):
+        #single_run(self, input_file_path, output_file_path, cmd_file_path, rule_list):
         pass
-
+# create check input files
 
 # run check
 
--- GAMESS/inputfiles.py
+++ GAMESS/inputfiles.py
@@ -1,41 +1,50 @@
 from enum import Enum
 from functools import reduce
 
+
 class ExeTyp(Enum):
-    Run = ('*ExeTyp*','Run')
-    Check = ('*ExeTyp*','Check')
+    Run = ('*ExeTyp*', 'Run')
+    Check = ('*ExeTyp*', 'Check')
+
 
 class RunTyp(Enum):
-    Energy = ('*RunTyp*','Energy')
-    Optimize = ('*RunTyp*','Optimize')
-    Hessian = ('*RunTyp*','Hessian')
+    Energy = ('*RunTyp*', 'Energy')
+    Optimize = ('*RunTyp*', 'Optimize')
+    Hessian = ('*RunTyp*', 'Hessian')
+
 
 class Method(Enum):
-    RHF = ('*Method*','')
-    DFT = ('*Method*','mplevl = 2')
-    MP2 = ('*Method*','dfttyp = b3lyp')
+    RHF = ('*Method*', '')
+    DFT = ('*Method*', 'mplevl = 2')
+    MP2 = ('*Method*', 'dfttyp = b3lyp')
+
 
 class InputFiles(object):
     def __init__(self, template, gamess_command_path, gamess_string):
         self.template = template
         self.gamess_command_path = gamess_command_path
         self.gamess_string = gamess_string
-    #def __init__(self, template_path):
-        # with open(template_path, 'r') as file:
-        #     self.template = file.read()
 
-    def GenerateInput(self, output_path, rule_list):
+    # def __init__(self, template_path):
+    # with open(template_path, 'r') as file:
+    #     self.template = file.read()
+
+    def create_input(self, output_path, rule_list):
         out = self.template
         out = reduce(lambda a, kv: a.replace(*kv), rule_list, out)
 
         with open(output_path, 'w') as file:
             file.write(out)
 
-    def GenerateStartCommand(self, output_path, input_file_name, output_file_name):
+    def create_run_command(self, output_path, input_file_name, output_file_name):
         # for windows:
-        out = "del restart\\*.dat \n del restart\\*.rst \n @CALL %s %s %s %s". format(self.gamess_command_path, input_file_name, self.gamess_string, output_file_name)
+        out = 'del restart\\*.dat \n del restart\\*.rst \n @CALL %s %s %s %s'.format(self.gamess_command_path,
+                                                                                     input_file_name,
+                                                                                     self.gamess_string,
+                                                                                     output_file_name)
         with open(output_path, 'w') as file:
             file.write(out)
+
 # input = InputFile('C:\\Users\\Public\\gamess-64\\template.inp')
 # rule_list = ExeTyp.Run.value, Method.DFT.value, RunTyp.Optimize.value, ('*Name*', 'name'), ('*Symmetry*', 'C1'), ('*Geometry*', 'geometry')
 # input.Generate('C:\\Users\\Public\\gamess-64\\output.inp', rule_list)
